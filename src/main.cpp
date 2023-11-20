da82e4b191e930b55a94ed43cf9ef8afab64525b
umar.techboy <umar.techboy@gmail.com>
2023-11-21 02:36:13 +0500

    Custom Channels and config

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,75 @@
 #include <Arduino.h>
 
-void setup() {
+#define NoOfChannels 6
+
+long lastDebug = 0;
+int lastAnalogRead[NoOfChannels] = {0};
+int deltas[NoOfChannels] = {0};
+
+long gains[NoOfChannels] = {10};
+long dampings[NoOfChannels] = {4};
+long maxes[NoOfChannels] = {10000};
+
+
+void setup()
+{
   Serial.begin(115200);
+  for(int i = 1 ; i < NoOfChannels; i++){
+    gains[i] = gains[0];
+    dampings[i] = dampings[0];
+    maxes[i] = maxes[0];
+
+  }
 }
 
-long  lastDbug  = 0;
-int lastA0 = 0;
-int delta = 0;
-void loop() {
-  // put your main code here, to run repeatedly:
-  int currentA0 = analogRead(A0) * 10;
-  delta += abs(currentA0 - lastA0);
-  lastA0 = currentA0;
-  delta -= 4;
-  if(delta < 0)
-    delta = 0;
-  else if (delta > 10000)
-    delta = 10000;
+void loop()
+{
+  if (Serial.available()){
+    String str = Serial.readStringUntil('\n');
+    String com = str.substring(0, str.indexOf("="));
+    long * array = 0;
+    String arg = str.substring(str.indexOf("=") + 1);
+    if (com.startsWith("gain"))
+      array = gains;
+    else if (com.startsWith("max"))
+      array = maxes;
+    else if (com.startsWith("damping"))
+      array = dampings;
+    else { // common commands
 
-  if (millis() - lastDbug > 100){
-    Serial.println(delta);
-    //Serial.print(',');
-    //Serial.println((delta > 500) ? 5000 : 0);
-    lastDbug = millis();
+    }
+    if (array != 0){
+      int index = com.substring(com.indexOf(" ") + 1).toInt();
+      if (index < 0 || index >= NoOfChannels)
+        return;
+      int lastValue = array[index];
+      arg.trim();
+      if (arg != "")
+        lastValue = arg.toInt();
+      array[index] = lastValue;
+    }
   }
-
+  for (int i = 0; i < NoOfChannels; i++)
+  {
+    // put your main code here, to run repeatedly:
+    int currentAnalogRead = analogRead(A0 + i) * gains[i];
+    deltas[i] += abs(currentAnalogRead - lastAnalogRead[i]);
+    lastAnalogRead[i] = currentAnalogRead;
+    deltas[i] -= dampings[i];
+    if (deltas[i] < 0)
+      deltas[i] = 0;
+    else if (deltas[i] > maxes[i])
+      deltas[i] = maxes[i];
+  }
+  if (millis() - lastDebug > 100)
+  {
+    Serial.print("deltas:");
+    for (int i = 0; i < NoOfChannels; i++)
+    {
+      Serial.print(deltas[i]);
+      Serial.print(",");
+    }
+    Serial.println();
+    lastDebug = millis();
+  }
 }
